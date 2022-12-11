5ea558096276fdf933e0b36406517498721b706b
ltong4867 <skylartreks2@gmail.com>
2022-12-11 15:51:59 -0500

    Add base and autonomous mode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@Autonomous
+public class AutoTest extends LinearOpMode {
+
+    //Robot robot = new Robot();
+    //static final double COUNTS_PER_MOTOR_REV = 28 ;
+    //static final double DRIVE_GEAR_REDUCTION = 13.7;
+    //static final double WHEEL_DIAMETER_INCHES = 4;
+    //static final double COUNTS_PER_INCH = 45.4545455;
+
+    public DcMotor IntakeMotor = null;
+    ElapsedTime runtime = new ElapsedTime();
+    @Override
+    public void runOpMode() {
+        //robot.init(this.hardwareMap);
+/*
+        waitForStart();
+        double distanceInches = 12;
+        int leftTarget = (int) (distanceInches * COUNTS_PER_INCH);
+        int rightTarget = (int)(distanceInches * COUNTS_PER_INCH);
+        robot.drive(1, leftTarget);
+ */
+        IntakeMotor = hardwareMap.get(DcMotor.class, "IntakeMotor");
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive() && runtime.seconds() < 1.0) {
+            IntakeMotor.setPower(1);
+        } while (opModeIsActive() && runtime.seconds() < 2.0) {
+            IntakeMotor.setPower(-1);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp
+public class TeleOP extends LinearOpMode {
+    //Robot robot = new Robot();   // Use Robot's feature
+    //double dumperPos = 0;
+
+        //robot.init(this.hardwareMap);
+/*
+        waitForStart();
+        double distanceInches = 12;
+        int leftTarget = (int) (distanceInches * COUNTS_PER_INCH);
+        int rightTarget = (int)(distanceInches * COUNTS_PER_INCH);
+        robot.drive(1, leftTarget);
+ */
+        public DcMotor IntakeMotor = null;
+
+    @Override
+    public void runOpMode() {
+       // robot.init(this.hardwareMap);
+
+        //telemetry.addData("Mode", "waiting");
+        //telemetry.update();
+            IntakeMotor = hardwareMap.get(DcMotor.class, "IntakeMotor");
+        waitForStart();
+
+
+        while (opModeIsActive()) {
+            IntakeMotor.setPower(gamepad1.left_stick_y);
+        }
+            /*
+            double drive = -gamepad1.right_stick_y;
+            double strafe = -gamepad1.right_stick_x;
+            double turn = -gamepad1.left_stick_x * 0.8;
+
+
+
+    /*        if(Math.abs(strafe)<0.2){
+                strafe=0;
+            } */
+/*
+            double FLPower = Range.clip(drive - strafe + turn, -1.0, 1.0);
+            double FRPower = Range.clip(drive + strafe - turn, -1.0, 1.0);
+            double BLPower = Range.clip(drive + strafe + turn, -1.0, 1.0);
+            double BRPower = Range.clip(drive - strafe - turn, -1.0, 1.0);
+
+            robot.FrontLeftDrive.setPower(FLPower);
+            robot.FrontRightDrive.setPower(FRPower);
+            robot.RearLeftDrive.setPower(BLPower);
+            robot.RearRightDrive.setPower(BRPower);
+        }
+        */
+    }
+}
+
